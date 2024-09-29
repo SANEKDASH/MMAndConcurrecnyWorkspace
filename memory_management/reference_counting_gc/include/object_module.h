c69da54d9ef0f26ad5a8abe61ef1188fe7079d2d
Mikhail Molotkov <molotkov.md@phystech.edu>
2024-09-29 17:25:39 +0300

    Added refcountgc tests
    Signed-off-by: Mikhail Molotkov <molotkov.md@phystech.edu>

--- memory_management/reference_counting_gc/include/object_module.h
+++ memory_management/reference_counting_gc/include/object_module.h
@@ -1,7 +1,57 @@
-#ifndef MEMORY_MANAGEMENT_BUMP_POINTER_ALLOCATOR_INCLUDE_BUMP_POINTER_ALLOCATOR_H
-#define MEMORY_MANAGEMENT_BUMP_POINTER_ALLOCATOR_INCLUDE_BUMP_POINTER_ALLOCATOR_H
+#ifndef MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
+#define MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
+
+#include <cstddef>
 
 template <class T>
-class Object {};
+class Object;
 
-#endif  // MEMORY_MANAGEMENT_BUMP_POINTER_ALLOCATOR_INCLUDE_BUMP_POINTER_ALLOCATOR_H
+template <class T, class... Args>
+static Object<T> MakeObject(Args... args)
+{
+    // TODO(you): Implemet this finction
+    return {};
+}
+
+template <class T>
+class Object {
+public:
+    Object() = default;
+    Object([[maybe_unused]] T *ptr) {}
+
+    ~Object() {}
+
+    // copy semantic
+    Object([[maybe_unused]] Object<T> &other) {}
+    Object<T> &operator=([[maybe_unused]] Object<T> &other)
+    {
+        return {};
+    }
+
+    // move semantic
+    Object([[maybe_unused]] Object<T> &&other) {}
+    Object<T> &operator=([[maybe_unused]] Object<T> &&other)
+    {
+        return {};
+    }
+
+    // member access operators
+    T &operator*() const noexcept {}
+    T *operator->() const noexcept {}
+
+    // internal access
+    void reset([[maybe_unused]] T *ptr) noexcept {}
+    T *get() const noexcept
+    {
+        return nullptr;
+    }
+    size_t use_count() const noexcept
+    {
+        return 0;
+    }
+
+private:
+    // TODO(you): Add your fields and methods here...
+};
+
+#endif  // MEMORY_MANAGEMENT_REFERECNCE_COUNTING_GC_INCLUDE_OBJECT_MODEL_H
--- memory_management/reference_counting_gc/tests/gc_test.cpp
+++ memory_management/reference_counting_gc/tests/gc_test.cpp
@@ -1,7 +1,75 @@
 #include <gtest/gtest.h>
 #include "memory_management/reference_counting_gc/include/object_module.h"
 
-TEST(ReferenceCountingGC, DefaultObjectUsage)
+class Return42 {
+public:
+    size_t get() const
+    {
+        return 42U;
+    }
+};
+
+TEST(ReferenceCountingGC, SinglePtrUsage)
 {
     Object<size_t> obj;
+    ASSERT_EQ(obj.use_count(), 0);
+    ASSERT_EQ(obj.get(), nullptr);
+
+    constexpr size_t VALUE_TO_CREATE = 42U;
+    Object<size_t> sizeObj = MakeObject<size_t>(VALUE_TO_CREATE);
+    ASSERT_EQ(sizeObj.use_count(), 1U);
+    ASSERT_NE(sizeObj.get(), nullptr);
+    ASSERT_EQ(*sizeObj, VALUE_TO_CREATE);
+
+    Object<Return42> classObj = MakeObject<Return42>();
+    ASSERT_EQ(classObj.use_count(), 1U);
+    ASSERT_NE(classObj.get(), nullptr);
+    ASSERT_EQ(classObj->get(), Return42().get());
 }
+
+TEST(ReferenceCountingGC, CopySemanticUsage)
+{
+    constexpr size_t VALUE_TO_CREATE = 42U;
+    Object<size_t> obj1 = MakeObject<size_t>(VALUE_TO_CREATE);
+    {
+        Object<size_t> obj2(obj1);
+        ASSERT_EQ(obj1.use_count(), 2U);
+        ASSERT_EQ(obj1.get(), obj2.get());
+    }
+    ASSERT_EQ(obj1.use_count(), 1U);
+    {
+        Object<size_t> obj2;
+        obj2 = obj1;
+        ASSERT_EQ(obj1.use_count(), 2U);
+        ASSERT_EQ(obj1.get(), obj2.get());
+    }
+    ASSERT_EQ(obj1.use_count(), 1U);
+}
+
+TEST(ReferenceCountingGC, MoveSemanticUsage)
+{
+    constexpr size_t VALUE_TO_CREATE = 42U;
+    Object<size_t> obj1 = MakeObject<size_t>(VALUE_TO_CREATE);
+    size_t *ptr = obj1.get();
+    {
+        Object<size_t> obj2(std::move(obj1));
+        ASSERT_EQ(obj2.use_count(), 1U);
+        ASSERT_EQ(obj2.get(), ptr);
+        ASSERT_EQ(obj1.use_count(), 0);
+        ASSERT_EQ(obj1.get(), nullptr);
+        obj1 = obj2;
+    }
+    ASSERT_EQ(obj1.use_count(), 1U);
+    {
+        Object<size_t> obj2;
+        obj2 = std::move(obj1);
+        ASSERT_EQ(obj2.use_count(), 1U);
+        ASSERT_EQ(obj2.get(), ptr);
+        ASSERT_EQ(obj1.use_count(), 0);
+        ASSERT_EQ(obj1.get(), nullptr);
+        obj1 = obj2;
+    }
+    ASSERT_EQ(obj1.use_count(), 1U);
+}
+
+TEST(ReferenceCountingGC, GcDeletingTest) {}

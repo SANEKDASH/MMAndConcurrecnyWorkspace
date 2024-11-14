6b40f60a7dfb1e6fe8b237bb51cf0d4722fc5040
molotkovmikhail <molotkov.mikhail@huawei-partners.com>
2024-11-14 17:08:49 +0800

    lock free stack load test
    Signed-off-by: molotkovmikhail <molotkov.mikhail@huawei-partners.com>

--- concurrency/lock_free_stack/tests/lock_free_stack_tests.cpp
+++ concurrency/lock_free_stack/tests/lock_free_stack_tests.cpp
@@ -81,3 +81,56 @@ TEST(LockFreeStackTest, DISABLED_MultithreadingTest) {
 
     ASSERT_TRUE(queue.IsEmpty());
 }
+
+TEST(LockFreeStackTest, DISABLED_LoadTest) {
+    LockFreeStack<size_t> queue;
+    ASSERT_TRUE(queue.IsEmpty());
+    std::atomic<size_t> pushCounter = 0;
+    std::atomic<size_t> popCounter = 0;
+
+    std::vector<size_t> container;
+    std::mutex lock;
+
+    static constexpr size_t THREAD_COUNT = 10U;
+    static constexpr size_t PUSH_COUNT = 1'000'000U;
+
+    auto push = [&queue, &pushCounter](){
+        for(size_t i = 0; i < PUSH_COUNT; i++) {
+            queue.Push(i);
+        }
+    };
+
+    auto pop = [&queue, &popCounter]() {
+        while(popCounter.load() != THREAD_COUNT * PUSH_COUNT) {
+            auto val = queue.Pop();
+            if(val.has_value()) {
+                popCounter.fetch_add(1);
+            }
+        }
+    };
+
+    std::vector<std::thread> pushers;
+    std::vector<std::thread> poppers;
+    for(size_t i = 0; i < THREAD_COUNT; i++) {
+        pushers.emplace_back(push);
+        poppers.emplace_back(pop);
+    }
+
+    while(popCounter != THREAD_COUNT * PUSH_COUNT) {
+        // wait here
+    }
+    
+    for(auto& pusher : pushers) {
+        pusher.join();
+    }
+    for(auto& popper: poppers) {
+        popper.join();
+    }
+
+    std::sort(container.begin(), container.end());
+    for(size_t i = 0; i < THREAD_COUNT * PUSH_COUNT; i++) {
+        ASSERT_EQ(container[i], i);
+    }
+
+    ASSERT_TRUE(queue.IsEmpty());
+}

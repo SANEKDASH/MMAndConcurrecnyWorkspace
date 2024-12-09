2edeec8cc52e87fb5c0b35ac5928e6f770da6132
sanekdash <dashinsky_alexander92@mail.ru>
2024-12-09 09:47:01 +0300

    + lock free stack (without hazards)

--- concurrency/lock_free_stack/include/lock_free_stack.h
+++ concurrency/lock_free_stack/include/lock_free_stack.h
@@ -2,26 +2,60 @@
 #define CONCURRENCY_LOCK_FREE_STACK_CONTAINERS_INCLUDE_LOCK_FREE_STACK_H
 
 #include <optional>
+#include <stdio.h>
+
+template<class T>
+struct StackNode {
+    T data;
+    StackNode *prev;
+
+    StackNode(T val, StackNode *newPrev) : data(val), prev(newPrev) {}
+};
 
 template<class T>
 class LockFreeStack {
 public:
 
     void Push([[maybe_unused]] T val) {
-        // ... implement this
+        StackNode<T> *newNode = new StackNode<T> {val, nullptr};
+
+        while (true) {
+            newNode->prev = head.load(std::memory_order_release);
+
+            if (head.compare_exchange_weak(newNode->prev, newNode, std::memory_order_acquire)) {
+                break;
+            }
+        }
     }
 
     std::optional<T> Pop() {
-        // ... implement this
-        return T{};
+        StackNode<T> *oldHead;
+
+        T valueToReturn;
+
+        do {
+            oldHead = head.load(std::memory_order_release);
+
+            if (oldHead == nullptr) {
+                return std::nullopt;
+            }
+
+            valueToReturn = oldHead->data;
+
+            if (head.compare_exchange_weak(oldHead, oldHead->prev, std::memory_order_acquire)) {
+                break;
+            }
+        } while (true);
+
+        return valueToReturn;
     }
 
     bool IsEmpty() {
-        // ... implement this
-        return true;
+        return head == nullptr;
     }
 
 private:
+    std::atomic<StackNode<T> *> head = nullptr;
 };
 
 #endif
